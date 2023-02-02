da125bfb96653e7928bd8ad659e20a039985f01c
nick <nickmorus@gmail.com>
2023-02-03 00:30:18 +0300

    Добавьте файлы проекта.

---
+++ MyWidget/Models/SystemUptimeMonitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWidget.Models
+{
+    public class SystemUptimeMonitor
+    {
+        public event Action<TimeSpan>? UptimeChanged;
+        public void Start()
+        {
+            Task.Run(async () =>
+            {
+                TimeSpan uptime = GetUpTime();
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    uptime = uptime.Add(TimeSpan.FromSeconds(1));
+                    UptimeChanged?.Invoke(uptime);
+                }
+            });
+        }
+
+        private static TimeSpan GetUpTime()
+        {
+            using (var uptime = new PerformanceCounter("System", "System Up Time"))
+            {
+                uptime.NextValue();
+                return TimeSpan.FromSeconds(uptime.NextValue());
+            }
+        }
+    }
+}
---
+++ MyWidget/ViewModels/MyWidgetViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MyWidget.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyWidget.ViewModels
+{
+    internal class MyWidgetViewModel : BindableBase
+    {
+        private SystemUptimeMonitor monitor = new();
+        public MyWidgetViewModel()
+        {
+            monitor.Start();
+            monitor.UptimeChanged += (uptime)=> SystemUpTime = uptime.ToString("hh\\:mm\\:ss");
+        }
+
+        private string systemUptime = "Loading...";
+        public string SystemUpTime
+        {
+            set => SetProperty<string>(ref systemUptime, value); 
+            get => systemUptime;
+        }
+    }
+}
